bc7dfba41a40d4ca527049380280f2f436fe0fcd
Wesa-3 <itswesaqm@gmail.com>
2026-09-25 17:24:50 -0500

    Creación de la clase y pruebas quick sort 3

--- src/main/scala/QuickSort3.scala
+++ src/main/scala/QuickSort3.scala
@@ -1,3 +1,30 @@
 class QuickSort3 {
+    
+}
+import scala.annotation.tailrec
+object QuickSort3 {
+  @tailrec
+  def separar3(inputList: List[Int], p: Int, menoresAcc: List[Int] = Nil, igualesAcc: List[Int] = Nil, mayoresAcc: List[Int] = Nil): (List[Int], List[Int], List[Int]) = {
+    inputList match {
+      case Nil =>
+        (menoresAcc.reverse, igualesAcc.reverse, mayoresAcc.reverse)
+      case head :: tail =>
+        if (head < p)
+          separar3(tail, p, head :: menoresAcc, igualesAcc, mayoresAcc)
+        else if (head == p)
+          separar3(tail, p, menoresAcc, head :: igualesAcc, mayoresAcc)
+        else
+          separar3(tail, p, menoresAcc, igualesAcc, head :: mayoresAcc)
+    }
+  }
 
+  def quickSort3(inputList: List[Int]): List[Int] = {
+    inputList match {
+      case Nil => Nil
+      case _ :: _ =>
+        val pivote = inputList.head
+        val (menores, iguales, mayores) = separar3(inputList, pivote)
+        quickSort3(menores) ::: iguales ::: quickSort3(mayores)
+    }
+  }
 }
